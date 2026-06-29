efbb5d1e1d9d2563410247a7db7dd90117fb1f4d
Aman Saifi <amansaifi0019@gmail.com>
2026-06-29 05:54:52 +0530

    Day 6 all set

---
+++ day 6/code-snippets.php
@@ -0,0 +1,15 @@
+# Day 5
+
+## Topics Learned
+
+- Taxonomies
+- Categories vs Tags
+- Custom Taxonomies
+- Taxonomy Queries
+
+## Key Learnings
+
+- Taxonomies organize content.
+- Categories are hierarchical.
+- Tags are non-hierarchical.
+- Custom taxonomies help structure CPTs.
